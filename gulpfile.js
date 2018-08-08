a6f64d9498c278a8062cc15a36c919749444f795
Ivan Artamonov <i.o.artamonov@gmail.com>
2018-08-08 20:43:25 +0300

    Basic sass & js processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,49 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
+var gulp            = require('gulp'),
+    sass            = require('gulp-sass'),
+    concat          = require('gulp-concat'),
+    uglifyjs        = require('gulp-uglifyjs'),
+    cssnano         = require('gulp-cssnano'),
+    rename          = require('gulp-rename'),
+    autoPrefixer    = require('gulp-autoprefixer'),
+    rm              = require('gulp-rimraf'),
+    browserSync     = require('browser-sync');
+
+
+// Очищаем папку с выхлопом
+gulp.task('rm', function () {
+    return gulp.src('dist/*', {read: false})
+        .pipe(rm());
+});
 
 gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.sass')
         .pipe(sass())
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('watch', function(){
-    return gulp.watch('app/scss/**/*.sass', ['sass']);
+gulp.task('scripts', function(){
+    return gulp.src([
+            'app/libs/jquery/dist/jquery.min.js',
+            'app/libs/bootstrap4/dist/js/bootstrap.min.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglifyjs())
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('browser-sync', function(){
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
+});
+
+gulp.task('default', ['rm', 'browser-sync', 'sass', 'scripts'], function(){
+    gulp.watch('app/scss/**/*.sass', ['sass']);
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
 });
