1cb39ff892fdcfd4e975ce0e47e4da9113f96d84
Ivan Artamonov <i.o.artamonov@gmail.com>
2018-08-11 15:13:45 +0300

    Libs styles

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp            = require('gulp'),
     uglifyjs        = require('gulp-uglifyjs'),
     cssnano         = require('gulp-cssnano'),
     rename          = require('gulp-rename'),
-    autoPrefixer    = require('gulp-autoprefixer'),
+    //autoPrefixer    = require('gulp-autoprefixer'),
     rm              = require('gulp-rimraf'),
     browserSync     = require('browser-sync');
 
@@ -15,14 +15,26 @@ gulp.task('rm', function () {
         .pipe(rm());
 });
 
-gulp.task('sass', function(){
+// Собираем стили
+gulp.task('sass', ['csslibs'], function(){
     return gulp.src('app/scss/**/*.sass')
         .pipe(sass())
         .pipe(cssnano())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css/'));
 });
 
+// Компилируем CSS разных библиотек в один файл и сжимаем его
+gulp.task('csslibs', function(){
+    return gulp.src([
+            'app/libs/bootstrap4/dist/css/bootstrap.min.css'
+        ])
+        .pipe(concat('libs.min.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css/'))
+});
+
+// Собираем JS
 gulp.task('scripts', function(){
     return gulp.src([
             'app/libs/jquery/dist/jquery.min.js',
